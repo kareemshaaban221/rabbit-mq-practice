5d9870002d9a16cf37d84e7b6c3371edd85c605d
kareemshaaban221 <kareemshaaban221@gmail.com>
2024-10-13 22:36:00 +0300

    [sender] make it in while loop

--- index.php
+++ index.php
@@ -6,4 +6,11 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 $sender = new Sender();
 $sender->declareQueue('hello');
-$sender->publishMessage($argv[1] ?? 'Hello World!');
+
+echo "\033[32mWelcome to the PHP AMQP Sender.\033[0m\n";
+echo "\033[33mPress Ctrl+C to exit.\033[0m\n";
+while (true) {
+    echo "Enter message: ";
+    $message = fgets(STDIN);
+    $sender->publishMessage($message);
+}
