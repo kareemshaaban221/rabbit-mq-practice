b820610831cb00a6e990c6112fea370270375497
kareemshaaban221 <kareemshaaban221@gmail.com>
2024-10-15 11:47:58 +0300

    comments

--- receiver.php
+++ receiver.php
@@ -1,10 +1,20 @@
 <?php
 
+// Use the Receiver class from the App namespace
 use App\Receiver;
 
+// Load the Composer autoloader
 require_once __DIR__ . '/vendor/autoload.php';
 
+// Create a new Receiver object
 $receiver = new Receiver();
+
+// Declare a queue named 'hello'
 $receiver->declareQueue('hello');
+
+// Declare a consumer on the queue
 $receiver->declareConsumer();
+
+// Start consuming messages from the queue
 $receiver->run();
+
--- src/Entity.php
+++ src/Entity.php
@@ -6,32 +6,85 @@ use PhpAmqpLib\Channel\AMQPChannel;
 use PhpAmqpLib\Connection\AbstractConnection;
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 
+/**
+ * Abstract class for RabbitMQ entities
+ */
 abstract class Entity
 {
 
+    /**
+     * Connection to RabbitMQ server
+     *
+     * @var AbstractConnection
+     */
     protected AbstractConnection $connection;
+
+    /**
+     * AMQP channel
+     *
+     * @var AMQPChannel
+     */
     protected AMQPChannel $channel;
+
+    /**
+     * Name of the queue
+     *
+     * @var string
+     */
     protected string $queueName;
+
+    /**
+     * Configuration for queue declaration
+     *
+     * @var array
+     */
     protected array $configs;
 
+    /**
+     * Constructor
+     */
     public function __construct()
     {
+        // Create a new connection to RabbitMQ server
         $this->connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+
+        // Create a new channel
         $this->channel = $this->connection->channel();
+
+        // Set configurations for queue declaration
         $this->setConfigs();
     }
 
+    /**
+     * Set configurations for queue declaration
+     */
     protected abstract function setConfigs();
 
+    /**
+     * Declare a queue
+     *
+     * @param string $queueName
+     *
+     * @return void
+     */
     public function declareQueue($queueName)
     {
+        // Declare a queue with the given name and configuration
         $this->channel->queue_declare($queueName, ...$this->configs);
+
+        // Set the queue name
         $this->queueName = $queueName;
     }
 
+    /**
+     * Destructor
+     */
     public function __destruct()
     {
+        // Close the channel
         $this->channel->close();
+
+        // Close the connection
         $this->connection->close();
     }
 }
